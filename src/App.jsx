32c96c4733fdcbd51a1744b76b443c20036136c4
Caio Duque <caioduque.dev@gmail.com>
2024-04-05 03:22:50 -0300

    add useState to inputs

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,38 @@ import { v4 as uuidv4 } from 'uuid'
 function App() {
 
   const [list, setList] = useState([{ title: "Tarefa 1", description: "Descrição 1", done: false }, { title: "Tarefa 2", description: "Descrição 2", done: false }, { title: "Tarefa 3", description: "Descrição 3", done: true }])
-
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputDesc, setInputDesc] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <>
       <Header />
       <div className="app-flex-container">
         <div className="input-container">
           <h2>Adicionar tarefa</h2>
-          <input type="text" name="" id="" placeholder="Título"/>
-          <input type="text" name="" id="" placeholder="Descrição"/>
+          <input value={inputTitle} onInput={e => setInputTitle(e.target.value)} type="text" name="" id="" placeholder="Título" />
+          <input value={inputDesc} onInput={e => setInputDesc(e.target.value)} type="text" name="" id="" placeholder="Descrição" />
           <div className="checkbox">
             <label htmlFor="completed">Tarefa concluída?</label>
-            <input type="checkbox" name="completed" id="" />
+            <input onChange={() => setIsChecked(!isChecked)} type="checkbox" name="completed" id="" />
           </div>
-          <input type="button" value="Adicionar" onClick={() => setList(
-            [...list, {
-              // dados estaticos para teste, substituir por valores nos inputs
-              title: "Tarefa adicionada", 
-              description: "Tarefa adicionada", 
-              done: false}]
-              )}/>
+          <input type="button" value="Adicionar" onClick={() => {
+            setList(
+              [...list, {
+                // dados estaticos para teste, substituir por valores nos inputs
+                title: inputTitle,
+                description: inputDesc,
+                done: isChecked
+              }])
+
+            // clear inputs
+            setInputTitle('');
+            setInputDesc('');
+            setIsChecked(false);
+
+
+
+          }} />
 
         </div>
         <div className='list-container'>
